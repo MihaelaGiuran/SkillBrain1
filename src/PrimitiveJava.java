b9e2a01d0ca437517292d7e703084a5afead9e88
WMK <niculescu.mm@gmail.com>
2024-03-07 21:23:52 +0200

    lectia1

---
+++ src/PrimitiveJava.java
@@ -0,0 +1,30 @@
+public class PrimitiveJava {
+    public static void main(String[] args) {
+
+        int numar;
+        numar = 5;
+
+        int numar2 = 6;
+        Integer numarClasa = 10;
+
+        float numarCuVirgula = 7.3f;
+        Float wrapperFloat =7.3f;
+
+        double numarCuPunct = 8.5;
+        Double wrapperPesteDouble = 9.5;
+
+
+        char unSingurCaracter = 'G';
+        Character caracter = 'A';
+
+        long numarFoarteMare = 122344566789L;
+
+        short numarMaiMic = (short) 1234;
+        boolean trueSauFalse = true;
+        boolean trueSauFalse2 = false;
+
+        byte valoareMaxima = -128;
+
+
+    }
+}
---
+++ src/learning/ClassSkill.java
@@ -0,0 +1,7 @@
+package learning;
+
+public class ClassSkill {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}

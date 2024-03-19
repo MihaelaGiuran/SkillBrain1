22796e24d2fcb4a7249ab3a43c37ae8099cd986a
WMK <niculescu.mm@gmail.com>
2024-03-19 09:41:24 +0200

    lectia1

--- src/learning/tema1/Punctul7.java
+++ src/learning/tema1/Punctul7.java
@@ -7,14 +7,45 @@ public class Punctul7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Introduceti nr");
+
+
+
+
+
         int numar = scanner.nextInt();
         int rezultat = adunare (numar);
         System.out.println("Rezutatul este:".concat(String.valueOf(rezultat)));
+
+
+        System.out.println("Introduceti nr2");
+        double numar2 = scanner.nextInt();
+        double rezultat2 = scadere (numar2);
+        System.out.println("Rezutatul este:" + rezultat2);
+
+        System.out.println("Introduceti nr3");
+        double numar3 = scanner.nextInt();
+        double rezultat3 = inmultire (numar3);
+        System.out.println("Rezutatul este:" + rezultat3);
+
+        System.out.println("Introduceti nr4");
+        int numar4 = scanner.nextInt();
+        int rezultat4 = impartire (numar4);
+        System.out.println("Rezutatul este:".concat(String.valueOf(rezultat4)));
+
+        System.out.println("Introduceti nr5");
+        int numar5 = scanner.nextInt();
+        int rezultat5 = modul (numar5);
+        System.out.println("Rezutatul este:".concat(String.valueOf(rezultat4)));
+
 
     }
     public static int adunare(int numar){
         return numar + 5;
     }
+    public static double scadere(double numar2) {return numar2 - -12.3;}
+    public static double inmultire(double numar3) {return numar3 * -3.2;}
+    public static int impartire(int numar4) {return numar4 /4;}
+    public static int modul(int numar5) {return numar5 %6;}
 
 
 }
---
+++ src/learning/tema1/Punctul8.java
@@ -0,0 +1,29 @@
+package learning.tema1;
+
+public class Punctul8 {
+    public static void main(String[] args) {
+
+        int x = 2;
+
+        int y = 12;
+
+
+
+        // y = x * y;
+
+        // y = x + y;
+
+
+
+
+
+
+
+        System.out.println(x);
+
+        System.out.println(y);
+
+    }
+
+
+}

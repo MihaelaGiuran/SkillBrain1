522ae188c533e4924d60faff26d22c5235d6f580
WMK <niculescu.mm@gmail.com>
2024-03-19 10:08:47 +0200

    tema 1

--- src/learning/sprint1403/Exercitii.java
+++ src/learning/sprint1403/Exercitii.java
@@ -4,10 +4,8 @@ import java.util.Scanner;
 
 public class Exercitii {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("introduceti text");
-        String textCitit= scanner.nextLine();
-        String eliminaSpatiiGoale=textCitit.trim();
-        System.out.println("Noul tau text: "+ eliminaSpatiiGoale);
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate"));
+
     }
 }
